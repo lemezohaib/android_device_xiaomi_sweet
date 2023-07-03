a3387876a8a6e7f2fbd2e2ed6103e66aaf155a49
lemezohaib <zhbkhan644@gmail.com>
2023-07-03 20:17:09 +0000

    sweet: ArrowOS Bringup

--- arrow_sweet.mk
+++ arrow_sweet.mk
@@ -11,14 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_sweet
+PRODUCT_NAME := arrow_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#ArrowOS Stuff
+ARROW_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+DEVICE_MAINTAINER := lemezohaib
